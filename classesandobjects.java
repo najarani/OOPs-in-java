ca453ec7c284f96b1ebb9a22a6cc0f4380ba5ceb
najarani <nirajanpoudel77@gmail.com>
2023-08-19 18:42:15 +0545

    Unfinish work summited

---
+++ classesandobjects.java
@@ -0,0 +1,24 @@
+//It was invented to help people solve real world problems through coding easily with great reference
+class Pen{
+    String color;
+    String type;
+    public void write(){
+        System.out.println("Writing Something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+}
+
+public class classesandobjects{
+    public static void main(String[] args) {
+       Pen pen = new Pen();
+       pen.color= "blue";
+       pen.type ="gel";
+       Pen pen1 = new Pen();
+       pen1.color = "Red";
+       pen1.type = "Ball";
+       pen.printColor();
+       pen1.printColor();
+    }
+}
