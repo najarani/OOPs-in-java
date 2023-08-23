49c89c2f8e407032fe22a08730523230d6bdca07
najarani <nirajanpoudel77@gmail.com>
2023-08-23 21:01:01 +0545

    finishing oops now

---
+++ interfacesAbstrac.java
@@ -0,0 +1,28 @@
+// interface Animal{
+//     public void walk();
+// }
+// interface Herbivore{
+
+// }
+// class Horse implements Animal,Herbivore{
+//    public void walk(){
+//         System.out.println("walks on 4 legs");
+//     }
+// }
+//about static
+class Student {
+    String name;
+    static String school;
+}
+
+
+public class interfacesAbstrac {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+      
+        student1.name = "elon musk";
+        System.out.println(student1.name);
+        
+
+    }
+}
