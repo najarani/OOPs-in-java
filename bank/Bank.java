a542044c6f0464859a46933b877bfa205ff4c7e0
najarani <nirajanpoudel77@gmail.com>
2023-08-23 19:34:49 +0545

    package in access modifiers

---
+++ bank/Bank.java
@@ -0,0 +1,23 @@
+package bank;
+class Account{
+    public String name;
+    protected String email;
+    private String password;
+    //getters and setters
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String pass){
+        this.password = pass;
+    }
+}
+public class Bank{
+public static void main(String[] args) {
+    Account accoun1 = new Account();
+    accoun1.name = "Nirajan Poudel";
+    accoun1.email = "nirajan@gamil.com";
+    accoun1.setPassword("Nirajan");
+    System.out.println(accoun1.getPassword());
+}
+}
